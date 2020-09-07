eba2ea08960a69920c8f4028a73fcc05fb8a046a
iampaavan <paavan014@gmail.com>
2020-09-06 20:18:55 -0700

    Add EC2 Tags

--- main.tf
+++ main.tf
@@ -28,6 +28,9 @@ resource "random_pet" "sg" {}
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
+  tags = {
+    Name = "GitHub-Actions-EC2"
+  }
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
